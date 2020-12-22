ec9870715fa9dda16408c6bd695e9b1241365c0f
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-22 15:03:57 -0600

    added code

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import M from "materialize-css";
+
+function Navbar() {
+    
+    useEffect(() => {
+        let sidenav = document.querySelector('#nav-mobile');
+        M.Sidenav.init(sidenav, {});
+    }, []);
+    
+    return (
+        <nav>
+            <div class="nav-wrapper">
+            <Link to="/" class="brand-logo">Google Books</Link>
+            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/search">Search</Link></li>
+                <li><Link to="/saved">Saved</Link></li>
+            </ul>
+            </div>
+        
+            <ul class="sidenav" id="nav-mobile">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/search">Search</Link></li>
+                <li><Link to="/saved">Saved</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
