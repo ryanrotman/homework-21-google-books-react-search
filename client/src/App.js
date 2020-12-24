85b960a82bbd31aed91b9b43fcf02ced498edf64
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-24 00:10:25 -0600

    added Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,33 @@ import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "materialize-css/dist/css/materialize.min.css";
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/search">
-          <Search />
-        </Route>
-        <Route exact path="/saved">
-          <Saved />
-        </Route>
-        <Route>
-          <NoMatch />
-        </Route>
-      </Switch>
+      <main>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/search">
+            <Search />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </BrowserRouter>
   );
 }
