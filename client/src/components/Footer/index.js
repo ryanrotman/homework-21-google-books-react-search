01d7f9e607e3a0ed856b0d27d39a3359f80940aa
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-24 00:11:45 -0600

    built footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="page-footer">
+            <div className="footer-copyright">
+                <div className="container center-align">
+                    Copyright © 2020 Ryan Rotman
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
