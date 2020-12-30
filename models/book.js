be872aaa9db9f95d1526c17239eb644b8e007f67
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-29 23:49:49 -0600

    removed date and added id to schema

--- models/book.js
+++ models/book.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+    id: { type: String, required: true },
     title: { type: String, required: true },
     authors: { type: Array, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
     link: { type: String, required: true },
-    date: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model("Book", bookSchema);
