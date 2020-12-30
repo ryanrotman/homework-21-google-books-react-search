c801dc5a68be3d2e503093338a8f69e78759c5dc
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-29 23:45:08 -0600

    changed class to className and added styling

--- client/src/components/SearchCard/index.js
+++ client/src/components/SearchCard/index.js
@@ -1,19 +1,20 @@
 import "./index.css";
 
-function SearchCard() {
+function SearchCard(props) {
     return (
-        <div class="col s12">
-            <div class="card horizontal">
-            <div class="card-image">
-                <img src="https://via.placeholder.com/100x190" />
+        <div className="col s12">
+            <div className="card horizontal">
+            <div className="card-image">
+                <img src={props.image} alt={props.title} />
             </div>
-            <div class="card-stacked">
-                <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information.</p>
+            <div className="card-stacked">
+                <div className="card-content">
+                <span className="card-title">{props.title} by {props.authors}</span>
+                <p>{props.description}</p>
                 </div>
-                <div class="card-action">
-                    <a class="waves-effect waves-light btn btn-search-card">View</a>
-                    <a class="waves-effect waves-light btn btn-search-card">Save</a>
+                <div className="card-action">
+                    <a className="waves-effect waves-light btn btn-search-card" href={props.link} target="_blank">View</a>
+                    <a className="waves-effect waves-light btn btn-search-card" onClick={() => props.onClick(props.key)}>Save</a>
                 </div>
             </div>
             </div>
