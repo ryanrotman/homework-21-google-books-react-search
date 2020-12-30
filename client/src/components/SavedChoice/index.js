affce9cbd33fef06486ec29b54d89cfc93d0e24c
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-29 23:42:29 -0600

    changed class to className and added styling

--- client/src/components/SavedChoice/index.js
+++ client/src/components/SavedChoice/index.js
@@ -3,18 +3,18 @@ import "./index.css";
 
 function SavedChoice() {
     return (
-        <div class="col s12 m6">
-            <h4 class="header center-align">Saved Books</h4>
-            <div class="card horizontal">
-            <div class="card-image">
-                <i class="far fa-save"></i>
+        <div className="col s12 m6">
+            <h4 className="header center-align">Saved Books</h4>
+            <div className="card horizontal">
+            <div className="card-image">
+                <i className="far fa-save"></i>
             </div>
-            <div class="card-stacked">
+            <div className="card-stacked">
                 {/* <div class="card-content">
                 <p>Click below to see your list of saved books.</p>
                 </div> */}
-                <div class="card-action">
-                <Link to="/saved">Click here to view your saved books</Link>
+                <div className="card-action">
+                <Link to="/saved" className="link-saved">Click here to view your saved books</Link>
                 </div>
             </div>
             </div>
