c38c5cfa6061f037ec39f3e30d18b0e6da65402c
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-02 16:23:54 -0600

    added handleDelete function

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -19,14 +19,34 @@ function Saved() {
 
     console.log("SAVED BOOKS: ", books)
 
+    function handleBookDelete(id) {
+        API.deleteBook(id)
+            .then(res => loadbooks())
+            .catch(err => console.log(err))
+    };
+
     return (
         <div>
             <Header />
             <div className="container">
                 <br />
                 <div id="saved-return-section">
-                    <h5>Saved Books</h5>
-                    <SavedCard />
+                    <h5>Saved Books:</h5>
+                    {books.length ? (
+                        books.map(book => (
+                            <SavedCard
+                                key={book._id}
+                                title={book.title}
+                                authors={book.authors}
+                                description={book.description}
+                                image={book.image}
+                                link={book.link}
+                                onClick={() => handleBookDelete(book._id)}
+                            />
+                        ))
+                    ) : (
+                        <h5 className="center-align"><strong>No Results to Display</strong></h5>
+                    )}
                 </div>
             </div>
         </div>
