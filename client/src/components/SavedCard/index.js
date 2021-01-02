a16a11c1de3d32a65567d7a76f0cbb1f85f9a6e9
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-02 16:24:21 -0600

    built cards from info pulled from database

--- client/src/components/SavedCard/index.js
+++ client/src/components/SavedCard/index.js
@@ -1,19 +1,20 @@
 import "./index.css";
 
-function SavedCard() {
+function SavedCard(props) {
     return (
         <div className="col s12">
             <div className="card horizontal">
             <div className="card-image">
-                <img src="https://via.placeholder.com/100x190" />
+                <img src={props.image} alt={props.title} />
             </div>
             <div className="card-stacked">
                 <div className="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information.</p>
+                <span className="card-title">{props.title} by {props.authors}</span>
+                <p>{props.description}</p>
                 </div>
                 <div className="card-action">
-                    <a className="waves-effect waves-light btn btn-saved-card">View</a>
-                    <a className="waves-effect waves-light btn btn-saved-card">Delete</a>
+                    <a className="waves-effect waves-light btn btn-saved-card" href={props.link} target="_blank">View</a>
+                    <a className="waves-effect waves-light btn btn-saved-card" onClick={() => props.onClick(props.key)}>Delete</a>
                 </div>
             </div>
             </div>
