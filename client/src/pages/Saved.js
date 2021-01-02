014109958044b50d3b970048124d2962d917ae35
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-02 16:02:54 -0600

    added loadbooks function to useEffect and update state

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import SavedCard from "../components/SavedCard";
+import API from "../utils/API";
 
 function Saved() {
+
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        loadbooks();
+    }, []);
+
+    function loadbooks() {
+        API.savedBooks()
+            .then(res => setBooks(res.data))
+            .catch(err => console.log(err))
+    };
+
+    console.log("SAVED BOOKS: ", books)
+
     return (
         <div>
             <Header />
